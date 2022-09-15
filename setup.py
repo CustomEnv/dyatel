09278d2fa2c3e278b954f41e865622ddc5e095f1
VladimirPodolyan <vladimir.podolyan64@gmail.com>
2022-09-15 15:23:28 +0300

    android fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='dyatel-wrapper',
-    version='1.2.5',
+    version='1.2.6',
     url='https://github.com/EnvInc/dyatel',
     packages=[
         'dyatel',
