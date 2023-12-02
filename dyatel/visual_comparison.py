16c3f8a3a3f1a381c662424be8d2c8bca2f12b14
VladimirPodolyan <vladimir.podolyan64@gmail.com>
2023-12-02 19:45:12 +0200

    initial commit

--- dyatel/visual_comparison.py
+++ dyatel/visual_comparison.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 import os
 import re
 import time
-import importlib
+import math
 import json
 import base64
+import importlib
 from urllib.parse import urljoin
 from typing import Union, List, Any
 from string import punctuation
@@ -14,6 +15,7 @@ import cv2.cv2 as cv2
 import numpy
 from skimage._shared.utils import check_shape_equality  # noqa
 from skimage.metrics import structural_similarity
+from PIL import Image
 
 from dyatel.exceptions import DriverWrapperException, TimeoutException
 from dyatel.js_scripts import add_element_over_js, delete_element_over_js
@@ -33,12 +35,16 @@ class VisualComparison:
     soft_visual_reference_generation = False
     default_delay = 0.75
     default_threshold = 0
+    dynamic_threshold_factor = 0
     diff_color_scheme = (0, 255, 0)
 
     def __init__(self, driver_wrapper, element):
         self.driver_wrapper = driver_wrapper
         self.dyatel_element = element
         self.screenshot_name = 'default'
+
+        if self.dynamic_threshold_factor and self.default_threshold:
+            raise Exception('Provide only one argument for threshold of visual comparison')
 
     def assert_screenshot(
             self,
@@ -135,6 +141,14 @@ class VisualComparison:
 
         return self
 
+    def calculate_threshold(self, reference_file: str):
+        img = Image.open(reference_file)
+        width, height = img.size
+        pixels_grid = height * width
+        threshold = self.dynamic_threshold_factor / math.sqrt(pixels_grid)
+        pixels_allowed = int(pixels_grid / 100 * threshold)
+        return threshold, f'\n{width}x{height}; {threshold=}; {pixels_allowed=} from {pixels_grid}'
+
     def _appends_dummy_elements(self, remove_data: list) -> VisualComparison:
         """
         Placed an element above each from given list and paints it black
@@ -191,6 +205,11 @@ class VisualComparison:
         """
         reference_image = cv2.imread(reference_file)
         output_image = cv2.imread(actual_file)
+        threshold = threshold if threshold else self.default_threshold
+
+        additional_data = ''
+        if not threshold:
+            threshold, additional_data = self.calculate_threshold(reference_file)
 
         try:
             check_shape_equality(reference_image, output_image)
@@ -216,7 +235,8 @@ class VisualComparison:
         if is_different:
             raise AssertionError(f"{base_error}:"
                                  f"\nThreshold is: {actual_threshold};"
-                                 f"\nPossible threshold is: {threshold}")
+                                 f"\nPossible threshold is: {threshold}"
+                                 + additional_data)
 
         return self
 
