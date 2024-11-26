ef3c1644a3123a8c27c1542c29acff501cd4f056
VladimirPodolyan <vladimir.podolyan64@gmail.com>
2024-11-26 19:05:26 +0100

    Appium version updated

--- dyatel/mixins/native_context.py
+++ dyatel/mixins/native_context.py
@@ -1,4 +1,4 @@
-from dyatel.mixins.objects.locator import *
+from dyatel.mixins.objects.locator import Locator
 
 
 class NativeContext:
@@ -15,20 +15,28 @@ class NativeContext:
 
 class NativeSafari:
 
+    ios_keyboard_hide_button = "//XCUIElementTypeButton[@name='Done']"
+    ios_mobile_small_bottom_bar_locator = '//*[@name="CapsuleViewController"]/XCUIElementTypeOther[1]'
+    ios_mobile_large_bottom_bar_locator = f'{ios_mobile_small_bottom_bar_locator}/XCUIElementTypeOther[1]' \
+                                          f'/XCUIElementTypeOther[2]'
+    ios_tablet_top_bar_locator = '//XCUIElementTypeOther[@name="UnifiedBar?isStandaloneBar=true"]' \
+                                 '/XCUIElementTypeOther[1]'
+    ios_mobile_top_bar_locator = '//*[contains(@name, "SafariWindow")]/XCUIElementTypeOther[1]' \
+                                 '/XCUIElementTypeOther/XCUIElementTypeOther'
+
     def __init__(self, driver_wrapper):
+        from dyatel.base.element import Element
         self.driver_wrapper = driver_wrapper
 
-        from dyatel.base.element import Element
+        self.top_bar = Element(
+            Locator(mobile=self.ios_mobile_top_bar_locator, tablet=self.ios_tablet_top_bar_locator),
+            name='safari top bar',
+            driver_wrapper=driver_wrapper
+        )
 
-        top_bar_locator = ios_mobile_top_bar_locator
-        if driver_wrapper.is_tablet:
-            top_bar_locator = ios_tablet_top_bar_locator
-
-        self.top_bar = Element(Locator(top_bar_locator, tablet=ios_tablet_top_bar_locator), name='safari top bar',
-                               driver_wrapper=driver_wrapper)
-        self.bottom_bar = Element(ios_mobile_small_bottom_bar_locator, name='safari bottom bar',
+        self.bottom_bar = Element(self.ios_mobile_small_bottom_bar_locator, name='safari bottom bar',
                                   driver_wrapper=driver_wrapper)
-        self.keyboard_done_button = Element(locator=ios_keyboard_hide_button, name='keyboard Done button',
+        self.keyboard_done_button = Element(locator=self.ios_keyboard_hide_button, name='keyboard Done button',
                                             driver_wrapper=driver_wrapper)
 
     def get_bottom_bar_height(self) -> int:
@@ -42,7 +50,7 @@ class NativeSafari:
 
         bottom_bar_height = self.bottom_bar.size.height
         if bottom_bar_height > 350:  # Large devices have different locator
-            self.bottom_bar.locator = ios_mobile_large_bottom_bar_locator
+            self.bottom_bar.locator = self.ios_mobile_large_bottom_bar_locator
             bottom_bar_height = self.bottom_bar.size.height
 
         return bottom_bar_height
