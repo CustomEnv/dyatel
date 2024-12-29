b62e5aada73aff3bc72274cf9a438cf328842e09
VladimirPodolyan <vladimir.podolyan64@gmail.com>
2024-12-29 14:57:07 +0100

    Fix test_hover for safari v2

--- tests/web_tests/test_element.py
+++ tests/web_tests/test_element.py
@@ -91,7 +91,7 @@ def test_type_clear_text_get_value(pizza_order_page):
 
 def test_hover(mouse_event_page):
     initial_not_displayed = not mouse_event_page.dropdown.is_displayed()
-    mouse_event_page.choose_language_button.scroll_into_view(sleep=0.3).hover()
+    mouse_event_page.choose_language_button.scroll_into_view(sleep=1).hover()
     after_hover_displayed = mouse_event_page.dropdown.wait_visibility_without_error().is_displayed()
     mouse_event_page.choose_language_button.hover_outside()
     after_outside_hover_displayed = not mouse_event_page.dropdown.wait_hidden().is_displayed()
